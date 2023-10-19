0df6b5473ed0837e1af8cee2efc8a64ecb90cbc6
Bruce <simple@example.com>
2023-10-19 20:59:42 +0800

    Add about window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QMessageBox>
+
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
@@ -6,10 +8,27 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
+    QAction *aboutAct = helpMenu->addAction(tr("&About"), this, &MainWindow::about);
+    aboutAct->setStatusTip(tr("Show the application's About box"));
+
+    QAction *aboutQtAct = helpMenu->addAction(tr("About &Qt"), qApp, &QApplication::aboutQt);
+    aboutQtAct->setStatusTip(tr("Show the Qt library's About box"));
+    //setWindowIcon(QIcon(":Pic/Tools.png"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About Application"),
+                       tr("The <b>Widget Test</b> example demonstrates how to "
+                          "write modern GUI applications using Qt, with a menu bar, "
+                          "toolbars, and a status bar. "
+                          "Credits: Icon made by pongsakornRed from https://www.flaticon.com/free-icons/tool-box"));
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void about();
 
 private:
     Ui::MainWindow *ui;
